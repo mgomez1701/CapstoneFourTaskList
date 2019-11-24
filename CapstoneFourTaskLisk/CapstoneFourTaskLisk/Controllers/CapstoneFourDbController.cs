6e3e86234ecd9c7f8cdafc26affad3332a7eddc0
Mike <michael005@me.com>
2019-11-24 17:06:46 -0500

    First Commit

---
+++ CapstoneFourTaskLisk/CapstoneFourTaskLisk/Controllers/CapstoneFourDbController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CapstoneFourTaskLisk.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CapstoneFourTaskLisk.Controllers
+{
+    public class CapstoneFourDbController : Controller
+    {
+        private readonly CapstoneFourDbContext _database;
+
+        public CapstoneFourDbController(CapstoneFourDbContext database)
+        {
+            _database = database;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewTaskList()
+        {
+            return View(_database.TaskList.ToList());
+        }
+
+
+    }
+}
---
+++ CapstoneFourTaskLisk/CapstoneFourTaskLisk/Models/TaskList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CapstoneFourTaskLisk.Models
+{
+    public partial class TaskList
+    {
+        public int TaskId { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool? Complete { get; set; }
+        public string UserId { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
